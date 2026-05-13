121dbf248cb8bd2ece6b6934da583604fe085204
Tae Hyoung Kang <kth1888@gmail.com>
2026-05-14 01:30:15 +0900

    feat: dust alert logic improvement

--- services/checkdustandalert.js
+++ services/checkdustandalert.js
@@ -25,36 +25,45 @@ async function checkDustAndAlert(client) {
             continue;
         }
 
-        const previousDustGrade = userConfig.munjiAlarm.currentDustGrade;
         const userDustGrade = cv.pm10Grade_whoStandard_eightLevel;
-        const canUserVentilate = userDustGrade < 5;
-        const isGradeChanged = previousDustGrade != userDustGrade ? true : false;
 
+        // 미세먼지 레벨이 없거나 0이거나 레벨에 해당하는 등급단위가 없는 경우
         if (!userDustGrade || userDustGrade == 0 || !levels[userDustGrade]) {
-            if (!isGradeChanged) {
-                continue;
-            } else {
+            if (userConfig.munjiAlarm.hasDataError !== true) {
                 await channel.send(`[안내] ${location} 지역은 현재 데이터를 불러올 수 없습니다.`);
-                userConfig.munjiAlarm.currentDustGrade = userDustGrade;
-                continue;
+                userConfig.munjiAlarm.hasDataError = true;
             }
-
+            continue;
         }
 
-        userConfig.munjiAlarm.currentDustGrade = userDustGrade;
-        if (previousDustGrade === null) {
-            if (canUserVentilate) {
-                await channel.send(`**${dustData.datatime}** \n** ${location} : 지금 환기 타임!!** **(${levels[userDustGrade].label} ${levels[userDustGrade].emoji})** `);
-            } else {
-                await channel.send(`**${dustData.datatime}** \n** ${location} : 지금 창문 닫아!!** **(${levels[userDustGrade].label} ${levels[userDustGrade].emoji})** `);
-            }
-        } else if (canUserVentilate && isGradeChanged) {
-                await channel.send(`**${dustData.datatime}** \n** ${location} : 지금 환기 타임!!** **(${levels[userDustGrade].label} ${levels[userDustGrade].emoji})** `);
-        } else if (!canUserVentilate && isGradeChanged) {
-                await channel.send(`**${dustData.datatime}** \n** ${location} : 지금 창문 닫아!!** **(${levels[userDustGrade].label} ${levels[userDustGrade].emoji})** `);
-            }
+        // 데이터가 정상인 경우 플래그 해제
+        userConfig.munjiAlarm.hasDataError = false;
+
+        // 판별식
+        const canUserVentilate = userDustGrade < 5;
+
+        if (userConfig.munjiAlarm.wasAirGood === undefined) {
+            userConfig.munjiAlarm.wasAirGood = canUserVentilate;
         }
-        db.saveUsers(users);
+        const wasAirGood = userConfig.munjiAlarm.wasAirGood;
+
+        // 기존에 저장된 불필요한 필드가 있다면 삭제 (마이그레이션)
+        if (userConfig.munjiAlarm.currentDustGrade !== undefined) {
+            delete userConfig.munjiAlarm.currentDustGrade;
+        }
+
+        const isStateChanged = (wasAirGood !== canUserVentilate);
+
+        if (isStateChanged) {
+            if (canUserVentilate && !wasAirGood) {
+                await channel.send(`**${dustData.datatime}** \n** ${location} : 지금 환기 타임!!** **(${levels[userDustGrade].label} ${levels[userDustGrade].emoji})** `);
+            } else if (!canUserVentilate && wasAirGood) {
+                await channel.send(`**${dustData.datatime}** \n** ${location} : 지금 창문 닫아!!** **(${levels[userDustGrade].label} ${levels[userDustGrade].emoji})** `);
+            }
+            userConfig.munjiAlarm.wasAirGood = canUserVentilate;
+        }
     }
+    db.saveUsers(users);
+}
 
  module.exports = { checkDustAndAlert };
